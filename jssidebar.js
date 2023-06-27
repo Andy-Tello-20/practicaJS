f47a4e03cccdd2710cda01ccf2b9fb7b4bb71e76
Andres Tello <andrest911@gmail.com>
2023-06-27 17:42:45 -0300

    tarjeta-formulario

--- jssidebar.js
+++ jssidebar.js
@@ -9,3 +9,15 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+let registrar = document.getElementById("registro")
+
+let contenedor =document.getElementsByClassName("contenedor")[0]
+
+
+
+registrar.addEventListener("click", () => {
+    contenedor.classList.add("visible")
+    nav.classList.remove("visible")
+})
